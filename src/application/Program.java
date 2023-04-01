b354eda336955e6381bcf88c871362ea72566cdf
Gabriel Giovani <gabrielgiovanesouza32@gmail.com>
2023-04-01 17:31:00 -0300

    Department entity class

---
+++ src/application/Program.java
@@ -0,0 +1,13 @@
+package application;
+
+import model.entities.Department;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Department d = new Department(1, "books");
+		System.out.println(d);
+		
+	}
+}
