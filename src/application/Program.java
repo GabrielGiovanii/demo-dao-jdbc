a2fb0410b9307302faa10270a9fb2e63c6122ee7
Gabriel Giovani <gabrielgiovanesouza32@gmail.com>
2023-04-01 17:46:54 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,18 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Department d = new Department(1, "books");
-		System.out.println(d);
+		Department obj = new Department(1, "books");
+		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
 		
+		System.out.println(seller);
+
 	}
 }
