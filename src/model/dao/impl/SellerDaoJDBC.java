a9999d2623fe76b6d7f34b37ad25b67ab18b7e6a
Gabriel Giovani <gabrielgiovanesouza32@gmail.com>
2023-06-14 13:11:22 -0300

    findAll implementation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -95,8 +95,45 @@ public class SellerDaoJDBC implements SellerDao {
 
 	@Override
 	public List<Seller> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		
+		try {
+			StringBuilder sb = new StringBuilder();
+			sb.append("SELECT s.Id, s.`Name`, s.Email, s.BirthDate, s.BaseSalary,");
+			sb.append("s.DepartmentId, d.`Name` AS DepartmentName ");
+			sb.append("FROM seller AS s ");
+			sb.append("INNER JOIN department AS d ");
+			sb.append("ON s.DepartmentId = d.Id ");
+			sb.append("ORDER BY s.`Name`;");
+		
+			ps = conn.prepareStatement(sb.toString());
+			
+			rs = ps.executeQuery();
+			
+			List<Seller> list = new ArrayList<>();
+			Map<Integer, Department> map = new HashMap<>();
+			
+			while(rs.next()) {
+				Department dep = map.get(rs.getInt("DepartmentId"));
+				
+				if(dep == null) {
+					dep = instantiateDepartment(rs);
+					map.put(rs.getInt("DepartmentId"), dep);
+				}
+				
+				Seller seller = instantiateSeller(rs, dep);
+				list.add(seller);
+			}
+			return list;
+		}
+		catch(Exception ex) {
+			throw new DbException(ex.getMessage());
+		}
+		finally {
+			DB.closeResultSet(rs);
+			DB.closeStatement(ps);
+		}
 	}
 
 	@Override
@@ -111,7 +148,8 @@ public class SellerDaoJDBC implements SellerDao {
 			sb.append("FROM seller AS s ");
 			sb.append("INNER JOIN department AS d ");
 			sb.append("ON s.DepartmentId = d.Id ");
-			sb.append("WHERE s.DepartmentId = ?;"); 
+			sb.append("WHERE s.DepartmentId = ? "); 
+			sb.append("ORDER BY s.`Name`;");
 		
 			ps = conn.prepareStatement(sb.toString());
 			ps.setInt(1, department.getId());
