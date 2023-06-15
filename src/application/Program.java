e46c3b37fa132b438755cf60d747a99c68a0a2d5
Gabriel Giovani <gabrielgiovanesouza32@gmail.com>
2023-06-15 20:41:13 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,10 @@
 package application;
 
+import java.security.Timestamp;
+import java.text.DateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -19,7 +24,7 @@ public class Program {
 		System.out.println(seller);
 		
 		System.out.println("\n===  TESTE 2: seller findByDepartment ===");
-		Department depTest = new Department(3, null);
+		Department depTest = new Department(4, null);
 		List<Seller> list = sellerDao.findByDepartment(depTest);
 		for(Seller obj : list) {
 			System.out.println(obj);
@@ -35,5 +40,9 @@ public class Program {
 		Seller newSeller = new Seller(null, "Vitor", "vitor@gmail.com", new Date(), 1500.5, depTest);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New id = " + newSeller.getId());
+		
+		System.out.println("\n===  TESTE 5: seller update ===");
+		Seller updateSeller = new Seller(12, "José", "jose@gmail.com", new Date(), 1900.99, depTest);
+		sellerDao.update(updateSeller);
 	}
 }
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -65,8 +65,30 @@ public class SellerDaoJDBC implements SellerDao {
 
 	@Override
 	public void update(Seller obj) {
-		// TODO Auto-generated method stub
+		PreparedStatement ps = null;
 		
+		try {
+			StringBuilder sb = new StringBuilder();
+			sb.append("UPDATE seller ");
+			sb.append("SET `Name` = ?, Email = ?, BirthDate = ?,");
+			sb.append("BaseSalary = ?, DepartmentId = ? ");
+			sb.append("WHERE seller.Id = ?;");
+			
+			ps = conn.prepareStatement(sb.toString());
+			
+			ps.setString(1, obj.getName());
+			ps.setString(2, obj.getEmail());
+			ps.setDate(3, new java.sql.Date(obj.getBirthDate().getTime()));
+			ps.setDouble(4, obj.getBaseSalary());
+			ps.setInt(5, obj.getDepartment().getId());
+			ps.setInt(6, obj.getId());
+			
+			ps.executeUpdate();
+		}
+		catch(SQLException ex) {
+			DB.closeStatement(ps);
+			throw new DbException(ex.getMessage());
+		}
 	}
 
 	@Override
