b0c0a6cb30c7f661e0f9af3005b06633b00d047c
Gabriel Giovani <gabrielgiovanesouza32@gmail.com>
2023-06-16 13:48:27 -0300

    insert implementation DepartmentDaoJDBC

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -1,9 +1,18 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
+import model.entities.Department;
+
 public class Program2 {
 
 	public static void main(String[] args) {
+		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
 		
+		System.out.println("\n===  TESTE 1: department insert ===");
+		Department department = new Department(null, "Manutenção");
+		departmentDao.insert(department);
+		System.out.println("Inserted! New id = " + department.getId());
 	}
 
 }
--- src/model/dao/impl/DepartmentDaoJDBC.java
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -1,8 +1,14 @@
 package model.dao.impl;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
 
+import db.DB;
+import db.DbException;
 import model.dao.DepartmentDao;
 import model.entities.Department;
 
@@ -16,8 +22,33 @@ public class DepartmentDaoJDBC implements DepartmentDao {
 	
 	@Override
 	public void insert(Department obj) {
-		// TODO Auto-generated method stub
+		PreparedStatement ps = null;
 		
+		try {
+			ps = conn.prepareStatement("INSERT INTO department (`Name`) VALUES(?);", 
+					Statement.RETURN_GENERATED_KEYS);
+			
+			ps.setString(1, obj.getName());
+			
+			int rowsAffected = ps.executeUpdate();
+			
+			if(rowsAffected > 0) {
+				ResultSet rs = ps.getGeneratedKeys();
+				if(rs.next()) {
+					obj.setId(rs.getInt(1));
+				}
+				DB.closeResultSet(rs);
+			}
+			else {
+				throw new DbException("Unexpected error! No rows afftected");
+			}
+		}
+		catch(SQLException ex) {
+			throw new DbException(ex.getMessage());
+		}
+		finally {
+			DB.closeStatement(ps);
+		}
 	}
 
 	@Override
